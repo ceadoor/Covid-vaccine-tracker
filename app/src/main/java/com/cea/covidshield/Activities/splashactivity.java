fe78596cb5eb9924efd50d298257d0fc29e49ecc
isaac3001 <isaacs3001@gmail.com>
2021-07-14 08:55:08 +0530

    splash screen

---
+++ app/src/main/java/com/cea/covidshield/Activities/splashactivity.java
@@ -0,0 +1,45 @@
+package com.cea.covidshield.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ProgressBar;
+
+import com.cea.covidshield.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashactivity extends AppCompatActivity {
+
+        ProgressBar progressBar;
+        int i = 0;
+
+
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+
+            getSupportActionBar().hide();
+            final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressbar);
+            progressBar.setProgress(i);
+            new CountDownTimer(5000, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    i++;
+                    progressBar.setProgress((int)i*100/(5000/1000));
+
+                }
+
+                @Override
+                public void onFinish() {
+                    Intent secondActivity = new Intent (getApplicationContext(), MainActivity.class);
+
+                    startActivity(secondActivity);
+                    i++;
+                    progressBar.setProgress(100);
+                }
+            }.start();
+
+        }
+}
